f05fa5387bfdb3e7b1e475bd580608604b6cb970
Kite0301 <kaito.titech@gmail.com>
2017-04-17 12:12:36 +0900

    Add Drink class

--- lesson4/index.rb
+++ lesson4/index.rb
@@ -12,11 +12,35 @@ class Food
   end
 end
 
+class Drink
+  attr_accessor :name
+  attr_accessor :price
+  attr_accessor :type
+
+  def initialize(name:, price:, type:)
+    self.name = name
+    self.price = price
+    self.type = type
+  end
+
+  def price_with_tax
+    return (self.price * 1.08).to_i
+  end
+end
+
 foods = [
   Food.new(name: "ハンバーガー", price: 500),
   Food.new(name: "唐揚げ定食", price: 700),
   Food.new(name: "海鮮丼", price: 1000),
 ]
+
+drinks = [
+  Drink.new(name: "オレンジジュース", price: 300, type: "アイス"),
+  Drink.new(name: "コーヒー", price: 400, type: "アイス"),
+  Drink.new(name: "コーヒー", price: 500, type: "ホット"),
+]
+
+total_price = 0
 
 puts "--- お食事 ---"
 
@@ -27,13 +51,41 @@ foods.each do |food|
 end
 
 puts "--------------"
-puts "メニューを選んでください"
+puts "メニューの番号を選択してください"
 
 food_order = gets.chomp.to_i
 food = foods[food_order]
 
-puts "金額: #{food.price}円"
-puts "税込価格: #{food.price_with_tax}円"
+if food
+  puts "お食事の注文を受け付けました"
+  total_price += food.price_with_tax
+else
+  puts "正しい番号が入力されませんでした"
+end
+
+puts "--- お飲み物 ---"
+
+order = 0
+drinks.each do |drink|
+  puts "#{order}. #{drink.name}"
+  order += 1
+end
+
+puts "--------------"
+puts "メニューの番号を選択してください"
+
+drink_order = gets.chomp.to_i
+drink = drinks[drink_order]
+
+if drink
+  puts "お飲み物の注文を受け付けました"
+  total_price += drink.price_with_tax
+else
+  puts "正しい番号が入力されませんでした"
+end
+
+puts "--------------"
+puts "合計金額は、#{total_price}円（税込）です"
 
 __END__
 
